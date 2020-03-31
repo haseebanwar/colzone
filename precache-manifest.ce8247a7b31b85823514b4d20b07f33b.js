62833e2700f8895ea77c93c7027d1c0a52338f23
Haseeb Anwar <haseebanwar99@gmail.com>
2020-03-31 19:25:49 +0500

    Updates

---
+++ precache-manifest.ce8247a7b31b85823514b4d20b07f33b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c909dc1b7a1fa4d94c0812b234e1cdcb",
+    "url": "/colzone/index.html"
+  },
+  {
+    "revision": "1a0a100cf9f438104377",
+    "url": "/colzone/static/css/2.c05fe3be.chunk.css"
+  },
+  {
+    "revision": "016276c29528c98d882a",
+    "url": "/colzone/static/css/main.d4d263b0.chunk.css"
+  },
+  {
+    "revision": "1a0a100cf9f438104377",
+    "url": "/colzone/static/js/2.03fec347.chunk.js"
+  },
+  {
+    "revision": "42c3c94a3e26be691c31b5d2b0c63158",
+    "url": "/colzone/static/js/2.03fec347.chunk.js.LICENSE"
+  },
+  {
+    "revision": "016276c29528c98d882a",
+    "url": "/colzone/static/js/main.7c9873a7.chunk.js"
+  },
+  {
+    "revision": "c73af4c99826b740ca85",
+    "url": "/colzone/static/js/runtime-main.16d1b8ce.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colzone/static/media/bg.77428cd4.svg"
+  }
+]);
